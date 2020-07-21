e6abdfdea6f15b88cb2e32e841ba59ee1643d6e1
Timothée Lebourdais <timothe.leb@gmail.com>
2020-07-21 20:56:51 +0200

    add privacy policy

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) =>
   res.render("home", { title: "Turn your Google Sheet into an API" })
 );
 
+router.get("/privacy-policy", (req, res) =>
+    res.render("privacy", { title: "Privacy Policy" })
+);
+
 module.exports = router;
