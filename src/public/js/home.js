97cdaa36179d9a8a29f71c9fefe8658147382569
Timothée Lebourdais <timothe.leb@gmail.com>
2020-07-22 13:24:03 +0200

    update homepage

---
+++ src/public/js/home.js
@@ -0,0 +1,87 @@
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS(
+    "particles-js",
+
+    {
+      particles: {
+        number: {
+          value: 30,
+          density: {
+            enable: true,
+            value_area: 800,
+          },
+        },
+        color: {
+          value: "#ffffff",
+        },
+        shape: {
+          type: "star",
+        },
+        opacity: {
+          value: 0.3,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false,
+          },
+        },
+        size: {
+          value: 4,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 20,
+            size_min: 0.1,
+            sync: false,
+          },
+        },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: "#ffffff",
+          opacity: 0.4,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          speed: 6,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200,
+          },
+        },
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: false,
+            mode: "repulse",
+          },
+          onclick: {
+            enable: false,
+            mode: "push",
+          },
+          resize: true,
+        },
+        modes: {},
+      },
+      retina_detect: true,
+      config_demo: {
+        hide_card: false,
+        background_color: "#b61924",
+        background_image: "",
+        background_position: "50% 50%",
+        background_repeat: "no-repeat",
+        background_size: "cover",
+      },
+    }
+  );
+});
